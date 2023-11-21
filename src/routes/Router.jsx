06b0b66aaad5c7d1420110a477eaeab28c349eda
kugimiya shun <s1290195@u-aizu.ac.jp>
2023-11-21 12:50:06 +0900

    scss設定完了

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route,Routes, Navigate } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { NotFound } from "../pages/NotFound";
 import { SignIn } from "../pages/SignIn";
@@ -10,27 +10,48 @@ import { EditTask } from "../pages/EditTask";
 import { SignUp } from "../pages/SignUp";
 import { EditList } from "../pages/EditList";
 
+// export const Router = () => {
+//   const auth = useSelector((state) => state.auth.isSignIn)
+
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route exact path="/signin" element={<SignIn/>} />
+//         <Route exact path="/signup" element={<SignUp/>} />
+//         {auth ? (
+//           <>
+//             <Route exact path="/" element={<Home/>} />
+//             <Route exact path="/task/new" element={<NewTask/>} />
+//             <Route exact path="/list/new" element={<NewList/>} />
+//             <Route exact path="/lists/:listId/tasks/:taskId" element={<EditTask/>} />
+//             <Route exact path="/lists/:listId/edit" element={<EditList/>} />
+//           </>
+//         ) : (
+//           <Route path="/signin" render={() => <Navigate to="signin" />} />
+//         )}
+//         <Route path="*" element={<NotFound/>} />
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+
+
 export const Router = () => {
-  const auth = useSelector((state) => state.auth.isSignIn)
+  const auth = useSelector((state) => state.auth.isSignIn);
 
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/signup" component={SignUp} />
-        {auth ? (
-          <>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/task/new" component={NewTask} />
-            <Route exact path="/list/new" component={NewList} />
-            <Route exact path="/lists/:listId/tasks/:taskId" component={EditTask} />
-            <Route exact path="/lists/:listId/edit" component={EditList} />
-          </>
-        ) : (
-          <Redirect to="/signin" />
-        )}
-        <Route component={NotFound} />
-      </Switch>
+      <Routes>
+        <Route exact path="/signin" element={<SignIn />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/" element={auth ? <Home /> : <Navigate to="/signin" />} />
+        <Route exact path="/task/new" element={auth ? <NewTask /> : <Navigate to="/signin" />} />
+        <Route exact path="/list/new" element={auth ? <NewList /> : <Navigate to="/signin" />} />
+        <Route exact path="/lists/:listId/tasks/:taskId" element={auth ? <EditTask /> : <Navigate to="/signin" />} />
+        <Route exact path="/lists/:listId/edit" element={auth ? <EditList /> : <Navigate to="/signin" />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};

ab04e61072b4d1a44844c3debe4a587a23a8e5a1
kugimiya shun <s1290195@u-aizu.ac.jp>
2023-11-23 13:17:14 +0900

    子コンポーネントから親コンポーネントへの受け渡しの検証

--- src/pages/TestDate.jsx
+++ src/pages/TestDate.jsx
@@ -1,5 +1,16 @@
+import React,{useState} from 'react';
 import CustomTimePicker from './TimePicker';
 
 export const TestDate = () => {
-  return <CustomTimePicker />;
+  const [value, setValue] = useState(null);
+  const changeValue = (newValue) =>{
+    setValue(newValue)
+    console.log(value);
+  }
+
+  return(
+    <div>
+      <CustomTimePicker changeValue = {changeValue} value={value}/>
+    </div>
+  )
 };
--- src/pages/TimePicker.jsx
+++ src/pages/TimePicker.jsx
@@ -3,21 +3,23 @@ import * as React from 'react';
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 
-export default function ControlledComponent() {
-  const [value, setValue] = React.useState(null);
+export default function ControlledComponent(props) {
+  const value = props.value
+  const changeValue = (newValue) =>{
+    props.changeValue(newValue);
+  }
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={['DatePicker']}>
         <DateTimePicker
           value={value}
-          onChange={(newValue) => setValue(newValue)}
+          onChange={(newValue) => changeValue(newValue)}
         />
       </DemoContainer>
-      {console.log(value)}
     </LocalizationProvider>
   );
 }
